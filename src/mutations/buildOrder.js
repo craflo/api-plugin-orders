5c264c65cbe6c11fc44fcf46b33bf3003e272b4d
Pulkit Garg <wittycodes@gmail.com>
2020-09-21 19:49:33 +0530

    fix: buildor

--- src/mutations/buildOrder.js
+++ src/mutations/buildOrder.js
@@ -1,21 +1,13 @@
+import nameko from 'node-nameko-client';
 import _ from "lodash";
 import SimpleSchema from "simpl-schema";
+import Logger from "@reactioncommerce/logger";
 import Random from "@reactioncommerce/random";
 import ReactionError from "@reactioncommerce/reaction-error";
+import getAnonymousAccessToken from "@reactioncommerce/api-utils/getAnonymousAccessToken.js";
 import buildOrderFulfillmentGroupFromInput from "../util/buildOrderFulfillmentGroupFromInput.js";
+import verifyPaymentsMatchOrderTotal from "../util/verifyPaymentsMatchOrderTotal.js";
 import { Order as OrderSchema, orderInputSchema, Payment as PaymentSchema, paymentInputSchema } from "../simpleSchemas.js";
-import nameko from 'node-nameko-client';
-
-
-const inputSchema = new SimpleSchema({
-  "order": orderInputSchema,
-  "payments": {
-    type: Array,
-    optional: true
-  },
-  "payments.$": paymentInputSchema
-});
-
 
 
 async function rmqPaymentsMakeOrder(order) {
@@ -41,25 +33,106 @@ async function rmqPaymentsMakeOrder(order) {
   });
 }
 
-// You can also use promises. Here's an example with promises & ES6 syntax:
 
-/*
-nameko.connect(nameko_config)
-  .then(
-      rpc => rpc.call('mailer', 'send_mail', ['foo@example.org', 'Hello!', 'It\'s been a lo-o-o-ong time.']);
-  )
-  .then(
-      result => console.log('Success: Result is', result);
-  )
-  .catch(
-      error => console.error('Oops! RPC error:', error.stack);
-  );
+const inputSchema = new SimpleSchema({
+  "order": orderInputSchema,
+  "payments": {
+    type: Array,
+    optional: true
+  },
+  "payments.$": paymentInputSchema
+});
+
+/**
+ * @summary Create all authorized payments for a potential order
+ * @param {String} [accountId] The ID of the account placing the order
+ * @param {Object} [billingAddress] Billing address for the order as a whole
+ * @param {Object} context - The application context
+ * @param {String} currencyCode Currency code for interpreting the amount of all payments
+ * @param {String} email Email address for the order
+ * @param {Number} orderTotal Total due for the order
+ * @param {Object[]} paymentsInput List of payment inputs
+ * @param {Object} [shippingAddress] Shipping address, if relevant, for fraud detection
+ * @param {String} shop shop that owns the order
+ * @returns {Object[]} Array of created payments
+ */
+async function createPayments({
+  accountId,
+  billingAddress,
+  context,
+  currencyCode,
+  email,
+  orderTotal,
+  paymentsInput,
+  shippingAddress,
+  shop
+}) {
+  // Determining which payment methods are enabled for the shop
+  const availablePaymentMethods = shop.availablePaymentMethods || [];
+
+  // Verify that total of payment inputs equals total due. We need to be sure
+  // to do this before creating any payment authorizations
+  verifyPaymentsMatchOrderTotal(paymentsInput || [], orderTotal);
+
+  // Create authorized payments for each
+  const paymentPromises = (paymentsInput || []).map(async (paymentInput) => {
+    const { amount, method: methodName } = paymentInput;
+
+    // Verify that this payment method is enabled for the shop
+    if (!availablePaymentMethods.includes(methodName)) {
+      throw new ReactionError("payment-failed", `Payment method not enabled for this shop: ${methodName}`);
+    }
+
+    // Grab config for this payment method
+    let paymentMethodConfig;
+    try {
+      paymentMethodConfig = context.queries.getPaymentMethodConfigByName(methodName);
+    } catch (error) {
+      Logger.error(error);
+      throw new ReactionError("payment-failed", `Invalid payment method name: ${methodName}`);
+    }
+
+    // Authorize this payment
+    const payment = await paymentMethodConfig.functions.createAuthorizedPayment(context, {
+      accountId, // optional
+      amount,
+      billingAddress: paymentInput.billingAddress || billingAddress,
+      currencyCode,
+      email,
+      shippingAddress, // optional, for fraud detection, the first shipping address if shipping to multiple
+      shopId: shop._id,
+      paymentData: {
+        ...(paymentInput.data || {})
+      } // optional, object, blackbox
+    });
+
+    const paymentWithCurrency = {
+      ...payment,
+      // This is from previous support for exchange rates, which was removed in v3.0.0
+      currency: { exchangeRate: 1, userCurrency: currencyCode },
+      currencyCode
+    };
+
+    PaymentSchema.validate(paymentWithCurrency);
+
+    return paymentWithCurrency;
+  });
+
+  let payments;
+  try {
+    payments = await Promise.all(paymentPromises);
+    payments = payments.filter((payment) => !!payment); // remove nulls
+  } catch (error) {
+    Logger.error("createOrder: error creating payments", error.message);
+    throw new ReactionError("payment-failed", `There was a problem authorizing this payment: ${error.message}`);
+  }
+
+  return payments;
 }
-*/
 
 /**
  * @method buildOrder
- * @summary builds an order, for the payment gateway
+ * @summary Places an order, authorizing all payments first
  * @param {Object} context - an object containing the per-request state
  * @param {Object} input - Necessary input. See SimpleSchema
  * @returns {Promise<Object>} Object with `order` property containing the created order
@@ -145,9 +218,7 @@ export default async function buildOrder(context, input) {
     return group;
   }));
 
-  const PaymentServiceOrder = await rmqPaymentsMakeOrder(order);
-  await appEvents.emit("afterPaymentOrderBuilt", {
-    createdBy: userId,
+  const payments = await createPayments({
     accountId,
     billingAddress,
     context,
@@ -159,9 +230,114 @@ export default async function buildOrder(context, input) {
     shop
   });
 
+  // Create anonymousAccessToken if no account ID
+  const fullToken = accountId ? null : getAnonymousAccessToken();
+
+  const now = new Date();
+
+  const order = {
+    _id: orderId,
+    accountId,
+    billingAddress,
+    cartId,
+    createdAt: now,
+    currencyCode,
+    discounts,
+    email,
+    ordererPreferredLanguage: ordererPreferredLanguage || null,
+    payments,
+    shipping: finalFulfillmentGroups,
+    shopId,
+    surcharges: orderSurcharges,
+    totalItemQuantity: finalFulfillmentGroups.reduce((sum, group) => sum + group.totalItemQuantity, 0),
+    updatedAt: now,
+    workflow: {
+      status: "new",
+      workflow: ["new"]
+    }
+  };
+
+  if (fullToken) {
+    const dbToken = { ...fullToken };
+    // don't store the raw token in db, only the hash
+    delete dbToken.token;
+    order.anonymousAccessTokens = [dbToken];
+  }
+
+  let referenceId;
+  const createReferenceIdFunctions = getFunctionsOfType("createOrderReferenceId");
+  if (!createReferenceIdFunctions || createReferenceIdFunctions.length === 0) {
+    // if the cart has a reference Id, and no custom function is created use that
+    if (_.get(cart, "referenceId")) { // we want the else to fallthrough if no cart to keep the if/else logic simple
+      ({ referenceId } = cart);
+    } else {
+      referenceId = Random.id();
+    }
+  } else {
+    referenceId = await createReferenceIdFunctions[0](context, order, cart);
+    if (typeof referenceId !== "string") {
+      throw new ReactionError("invalid-parameter", "createOrderReferenceId function returned a non-string value");
+    }
+    if (createReferenceIdFunctions.length > 1) {
+      Logger.warn("More than one createOrderReferenceId function defined. Using first one defined");
+    }
+  }
+
+  order.referenceId = referenceId;
+
+
+  // Apply custom order data transformations from plugins
+  const transformCustomOrderFieldsFuncs = getFunctionsOfType("transformCustomOrderFields");
+  if (transformCustomOrderFieldsFuncs.length > 0) {
+    let customFields = { ...(customFieldsFromClient || {}) };
+    // We need to run each of these functions in a series, rather than in parallel, because
+    // each function expects to get the result of the previous. It is recommended to disable `no-await-in-loop`
+    // eslint rules when the output of one iteration might be used as input in another iteration, such as this case here.
+    // See https://eslint.org/docs/rules/no-await-in-loop#when-not-to-use-it
+    for (const transformCustomOrderFieldsFunc of transformCustomOrderFieldsFuncs) {
+      customFields = await transformCustomOrderFieldsFunc({ context, customFields, order }); // eslint-disable-line no-await-in-loop
+    }
+    order.customFields = customFields;
+  } else {
+    order.customFields = customFieldsFromClient;
+  }
+
+  // Validate and save
+  OrderSchema.validate(order);
+  const PaymentServiceOrder = await rmqPaymentsMakeOrder(order);
+  //await Orders.insertOne(order);
+  await appEvents.emit("afterPaymentOrderBuilt", { createdBy: userId, order });
+
   return {
+    orders: [order],
     PaymentServiceOrder: PaymentServiceOrder,
     // GraphQL response gets the raw token
     token: fullToken && fullToken.token
   };
 }
+
+
+
+
+
+
+
+
+
+// You can also use promises. Here's an example with promises & ES6 syntax:
+
+/*
+nameko.connect(nameko_config)
+  .then(
+      rpc => rpc.call('mailer', 'send_mail', ['foo@example.org', 'Hello!', 'It\'s been a lo-o-o-ong time.']);
+  )
+  .then(
+      result => console.log('Success: Result is', result);
+  )
+  .catch(
+      error => console.error('Oops! RPC error:', error.stack);
+  );
+}
+*/
+
+
