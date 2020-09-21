47ec52bb6b044537d9f6a6603610cba4a38439c1
Pulkit Garg <wittycodes@gmail.com>
2020-09-21 09:44:42 +0530

    Update buildOrder.js

--- src/resolvers/Mutation/buildOrder.js
+++ src/resolvers/Mutation/buildOrder.js
@@ -1,1 +1,51 @@
 
+import {
+  decodeCartOpaqueId,
+  decodeFulfillmentMethodOpaqueId,
+  decodeOrderItemsOpaqueIds,
+  decodeShopOpaqueId
+} from "../../xforms/id.js";
+
+/**
+ * @name Mutation/buildOrder
+ * @method
+ * @memberof Payments/GraphQL
+ * @summary resolver for the buildOrder GraphQL mutation
+ * @param {Object} parentResult - unused
+ * @param {Object} args.input - an object of all mutation arguments that were sent by the client
+ * @param {Object} args.input.order - The order input
+ * @param {Object[]} args.input.payments - Payment info
+ * @param {String} [args.input.clientMutationId] - An optional string identifying the mutation call
+ * @param {Object} context - an object containing the per-request state
+ * @returns {Promise<Object>} PlaceOrderPayload
+ */
+export default async function buildOrder(parentResult, { input }, context) {
+  const { clientMutationId = null, order, payments } = input;
+  const { cartId: opaqueCartId, fulfillmentGroups, shopId: opaqueShopId } = order;
+
+  const cartId = opaqueCartId ? decodeCartOpaqueId(opaqueCartId) : null;
+  const shopId = decodeShopOpaqueId(opaqueShopId);
+
+  const transformedFulfillmentGroups = fulfillmentGroups.map((group) => ({
+    ...group,
+    items: decodeOrderItemsOpaqueIds(group.items),
+    selectedFulfillmentMethodId: decodeFulfillmentMethodOpaqueId(group.selectedFulfillmentMethodId),
+    shopId: decodeShopOpaqueId(group.shopId)
+  }));
+
+  const { orders, token } = await context.mutations.buildOrder(context, {
+    order: {
+      ...order,
+      cartId,
+      fulfillmentGroups: transformedFulfillmentGroups,
+      shopId
+    },
+    payments
+  });
+
+  return {
+    clientMutationId,
+    orders,
+    token
+  };
+}
